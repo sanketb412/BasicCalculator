9d7168845bd6f0edc4c0b8c15e52a3f1f71384cf
Shweta Singh <Shweta.Singh1@Nykaa.local>
2021-04-10 09:17:14 +0530

    Added code of square

--- calculator.sh
+++ calculator.sh
@@ -1,14 +1,16 @@
 
 fno=10
-sno=20
+sno=2
 
 sum=$(($fno+$sno))
 sub=$(($sno-$fno))
 mul=$(($fno*$sno))
 div=$(($fno/$sno))
 mod=$(($fno%$sno))
+sqr=$(($fno**$sno))
 echo $sum
 echo $sub
 echo $mul
 echo $div
 echo $mod
+echo $sqr
